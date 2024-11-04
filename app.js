39f1e991a42ebb21c7e2e8feee554fe9f1952e14
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-11-05 01:26:53 +0530

    updated app.js

---
+++ app.js
@@ -0,0 +1,14 @@
+const imgSlider = document.querySelector(".img-slider");
+const nextBtn = document.querySelector(".next-btn");
+const prevBtn = document.querySelector(".prev-btn");
+
+let indexSlider = 0;
+
+const slider = () => {
+  imgSlider.style.transform = "rotate($(indexSlider * 60)deg)";
+};
+
+nextBtn.addEventListener("click", () => {
+  indexSlider++;
+  slider();
+});
