daeb6ec3648d2f7e516bfe22f7a62fd9d852f4c9
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-11-05 14:43:01 +0530

    updated app.js

--- app.js
+++ app.js
@@ -1,25 +1,39 @@
 const imgSlider = document.querySelector(".img-slider");
-const items = document.querySelector(".item");
+const items = document.querySelectorAll(".item");
+const imgItems = document.querySelectorAll(".img-item");
 
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 
 let indexSlider = 0;
-
+let index = 0;
 const slider = () => {
-  imgSlider.style.transform = "rotate($(indexSlider * 60)deg)";
+  imgSlider.style.transform = `rotate(${indexSlider * 60}deg)`;
 
   items.forEach((item) => {
-    item.style.transform = "rotate($(indexSlider * -60)deg)";
+    item.style.transform = `rotate(${indexSlider * -60}deg)`;
   });
+
+  document.querySelector("img-item.active").classList.remove("active");
+  imgItems[index].classList.add("active");
 };
 
 nextBtn.addEventListener("click", () => {
   indexSlider++;
+  index++;
+  if (index > imgItems.length - 1) {
+    index = 0;
+  }
+
   slider();
 });
 
 prevBtn.addEventListener("click", () => {
   indexSlider--;
+  index--;
+  if (index < 0) {
+    index = imgItems.length - 1;
+  }
+
   slider();
 });
