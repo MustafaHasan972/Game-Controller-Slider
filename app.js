4d818ec82e07c372ca05d1b8a0a81998f1736345
Mustafa Hasan <mustafa2k8972@gmail.com>
2024-11-17 23:06:54 +0530

    updated file

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
 const imgSlider = document.querySelector(".img-slider");
-const items = document.querySelectorAll;
+const items = document.querySelectorAll(".item");
+const imgItems = document.querySelectorAll(".img-item");
 
 const nextBtn = document.querySelector(".next-btn");
 const prevBtn = document.querySelector(".prev-btn");
 
 let indexSlider = 0;
+let index = 0;
 
 const slider = () => {
   imgSlider.style.transform = `rotate(${indexSlider * 60}deg)`;
@@ -12,16 +14,22 @@ const slider = () => {
   items.forEach((item) => {
     item.style.transform = `rotate(${indexSlider * -60}deg)`;
   });
+
+  imgItems[index].classList.add("active");
 };
 
 nextBtn.addEventListener("click", () => {
   indexSlider++;
+
+  index++;
 
   slider();
 });
 
 prevBtn.addEventListener("click", () => {
   indexSlider--;
+
+  index--;
 
   slider();
 });
